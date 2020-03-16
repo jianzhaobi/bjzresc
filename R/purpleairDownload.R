1b09f9807c996502004affd20a08a0b53f4e8edf
Bijianzhao <bjz91@qq.com>
2020-03-16 12:37:47 -0400

    ver 0.1.13 - add secondary PurpleAir data

--- R/purpleairDownload.R
+++ R/purpleairDownload.R
@@ -8,7 +8,7 @@
 #' @param output.path the path to output CSV files.
 #' @param average get average of this many minutes, valid values: 10, 15, 20, 30, 60, 240, 720, 1440, "daily". "daily" is not recommended as the daily values can only be calculated at the UTC time.
 #' @param time.zone time zone specification to be used for the conversion, but "" is the current time zone, and "GMT" is UTC (Universal Time, Coordinated). Invalid values are most commonly treated as UTC, on some platforms with a warning. For more time zones, see \link{https://www.mathworks.com/help/thingspeak/time-zones-reference.html}.
-#' @param indoor whether includes indoor sites (FALSE by default).
+#' @param indoor whether includes indoor sites (TRUE by default).
 #' @param n.thread number of parallel threads used to download the data (1 by default).
 #'
 #' @examples
@@ -21,7 +21,7 @@
 #' @export
 
 
-purpleairDownload <- function(site.csv, start.date, end.date, output.path, average, time.zone = 'GMT', indoor = F, n.thread = 1) {
+purpleairDownload <- function(site.csv, start.date, end.date, output.path, average, time.zone = 'GMT', indoor = T, n.thread = 1) {
 
   if (!require('httpuv')) {
     install.packages('httpuv')
@@ -65,10 +65,11 @@ purpleairDownload <- function(site.csv, start.date, end.date, output.path, avera
 
   # Field names
   # Primary
-  fieldnames.pri.A <- c("PM1.0_CF_ATM_ug/m3_A","PM2.5_CF_ATM_ug/m3_A","PM10.0_CF_ATM_ug/m3_A","Uptime_Minutes_A","RSSI_dbm_A","Temperature_F_A","Humidity_%_A","PM2.5_CF_1_ug/m3_A")
-  fieldnames.pri.B <- c("PM1.0_CF_ATM_ug/m3_B","PM2.5_CF_ATM_ug/m3_B","PM10.0_CF_ATM_ug/m3_B","HEAP_B","ADC0_voltage_B","Atmos_Pres_B","Not_Used_B","PM2.5_CF_1_ug/m3_B")
+  fieldnames.pri.A <- c("PM1.0_CF_1_ug/m3_A","PM2.5_CF_1_ug/m3_A","PM10.0_CF_1_ug/m3_A","Uptime_Minutes_A","RSSI_dbm_A","Temperature_F_A","Humidity_%_A","PM2.5_CF_ATM_ug/m3_A")
+  fieldnames.pri.B <- c("PM1.0_CF_1_ug/m3_B","PM2.5_CF_1_ug/m3_B","PM10.0_CF_1_ug/m3_B","HEAP_B","ADC0_voltage_B","Atmos_Pres_B","Not_Used_B","PM2.5_CF_ATM_ug/m3_B")
   # Secondary
-  # fieldnames.sec <- c("0.3um/dl","0.5um/dl","1.0um/dl","2.5um/dl","5.0um/dl","10.0um/dl","PM1.0_CF_1_ug/m3","PM10_CF_1_ug/m3")
+  fieldnames.sec.A <- c("0.3um/dl_A","0.5um/dl_A","1.0um/dl_A","2.5um/dl_A","5.0um/dl_A","10.0um/dl_A","PM1.0_CF_ATM_ug/m3_A","PM10_CF_ATM_ug/m3_A")
+  fieldnames.sec.B <- c("0.3um/dl_B","0.5um/dl_B","1.0um/dl_B","2.5um/dl_B","5.0um/dl_B","10.0um/dl_B","PM1.0_CF_ATM_ug/m3_B","PM10_CF_ATM_ug/m3_B")
 
   #--------------Run--------------#
   # For each site
@@ -82,10 +83,12 @@ purpleairDownload <- function(site.csv, start.date, end.date, output.path, avera
           name <- trimws(sites$Label[i]) # Remove Leading/Trailing Whitespace
           Lat <- sites$Lat[i]
           Lon <- sites$Lon[i]
-          # Channel A
+          # Channel A (Primary)
           ID.A <- sites$ID[i]
           channelID.A <- sites$THINGSPEAK_PRIMARY_ID[i]
           channelKey.A <- sites$THINGSPEAK_PRIMARY_ID_READ_KEY[i]
+          channelID.A.sec <- sites$THINGSPEAK_SECONDARY_ID[i]
+          channelKey.A.sec <- sites$THINGSPEAK_SECONDARY_ID_READ_KEY[i]
           # Channel B
           ib <- which(sites$ParentID == ID.A)
           if (length(ib) > 1) { # If there are multiple Channel B
@@ -93,9 +96,13 @@ purpleairDownload <- function(site.csv, start.date, end.date, output.path, avera
             ib.min <- ib.min[1] # If there are multiple channel B with the same AGE, select the first one
             channelID.B <- sites$THINGSPEAK_PRIMARY_ID[ib.min]
             channelKey.B <- sites$THINGSPEAK_PRIMARY_ID_READ_KEY[ib.min]
+            channelID.B.sec <- sites$THINGSPEAK_SECONDARY_ID[ib.min]
+            channelKey.B.sec <- sites$THINGSPEAK_SECONDARY_ID_READ_KEY[ib.min]
           } else {
             channelID.B <- sites$THINGSPEAK_PRIMARY_ID[ib]
             channelKey.B <- sites$THINGSPEAK_PRIMARY_ID_READ_KEY[ib]
+            channelID.B.sec <- sites$THINGSPEAK_SECONDARY_ID[ib]
+            channelKey.B.sec <- sites$THINGSPEAK_SECONDARY_ID_READ_KEY[ib]
           }
 
           print(ID.A)
@@ -116,8 +123,10 @@ purpleairDownload <- function(site.csv, start.date, end.date, output.path, avera
             # --- URL ---
             # Channel A
             url.csv.A <- paste("https://thingspeak.com/channels/", channelID.A, "/feed.csv?api_key=", channelKey.A, '&average=', average, "&round=3&start=", starttime, "&end=", endtime, '&timezone=', timezone, sep = '')
+            url.csv.A.sec <- paste("https://thingspeak.com/channels/", channelID.A.sec, "/feed.csv?api_key=", channelKey.A.sec, '&average=', average, "&round=3&start=", starttime, "&end=", endtime, '&timezone=', timezone, sep = '')
             # Channel B
             url.csv.B <- paste("https://thingspeak.com/channels/", channelID.B, "/feed.csv?api_key=", channelKey.B, '&average=', average, "&round=3&start=", starttime, "&end=", endtime, '&timezone=', timezone, sep = '')
+            url.csv.B.sec <- paste("https://thingspeak.com/channels/", channelID.B.sec, "/feed.csv?api_key=", channelKey.B.sec, '&average=', average, "&round=3&start=", starttime, "&end=", endtime, '&timezone=', timezone, sep = '')
 
             # --- Load CSV data ---
             # Download URL A
@@ -125,6 +134,14 @@ purpleairDownload <- function(site.csv, start.date, end.date, output.path, avera
             while (url.idx) {
               try.txt <- try(expr = { # Try to connect the link
                 dat.A <- read.csv(url.csv.A)
+              }, silent = T)
+              closeAllConnections()
+              if (!inherits(try.txt, 'try-error')) url.idx <- F
+            }
+            url.idx <- T
+            while (url.idx) {
+              try.txt <- try(expr = { # Try to connect the link
+                dat.A.sec <- read.csv(url.csv.A.sec)
               }, silent = T)
               closeAllConnections()
               if (!inherits(try.txt, 'try-error')) url.idx <- F
@@ -140,17 +157,33 @@ purpleairDownload <- function(site.csv, start.date, end.date, output.path, avera
                 closeAllConnections()
                 if (!inherits(try.txt, 'try-error')) url.idx <- F
               }
+              url.idx <- T
+              while (url.idx) {
+                try.txt <- try(expr = { # Try to connect the link
+                  dat.B.sec <- read.csv(url.csv.B.sec)
+                }, silent = T)
+                closeAllConnections()
+                if (!inherits(try.txt, 'try-error')) url.idx <- F
+              }
             } else {
               dat.B <- dat.A
+              dat.B.sec <- dat.A.sec
               if (nrow(dat.B) != 0) {
                 dat.B[,] <- NA
+                dat.B.sec[,] <- NA
                 dat.B$created_at <- dat.A$created_at
+                dat.B.sec$created_at <- dat.A.sec$created_at
               }
             }
-            # Combine Channel A & B
-            dat <- merge(dat.A, dat.B, by = c('created_at'), all = T)
             # Change the column names
-            names(dat)[2 : ncol(dat)] <- c(fieldnames.pri.A, fieldnames.pri.B)
+            names(dat.A)[2 : ncol(dat.A)] <- c(fieldnames.pri.A)
+            names(dat.A.sec)[2 : ncol(dat.A.sec)] <- c(fieldnames.sec.A)
+            names(dat.B)[2 : ncol(dat.B)] <- c(fieldnames.pri.B)
+            names(dat.B.sec)[2 : ncol(dat.B.sec)] <- c(fieldnames.sec.B)
+            # Combine Channel A & B
+            dat <- merge(dat.A, dat.A.sec, by = c('created_at'), all = T)
+            dat <- merge(dat, dat.B, by = c('created_at'), all = T)
+            dat <- merge(dat, dat.B.sec, by = c('created_at'), all = T)
 
             # --- Combine data frame ---
             dat.final <- rbind(dat.final, dat)
@@ -177,9 +210,3 @@ purpleairDownload <- function(site.csv, start.date, end.date, output.path, avera
   #--------------Run--------------#
 
 }
-
-
-
-
-
-
